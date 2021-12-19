57be1e5bfa1ec652c8aec1c2d0c7f8feaecf8c41
CYRIL  K  SONY <84973835+CyrilKSony@users.noreply.github.com>
2021-12-18 23:02:25 -0800

    Sum of two integers

---
+++ Sum.java
@@ -0,0 +1,12 @@
+package program_1; 
+import java.util.Scanner; 
+public class Addition { 
+public static void main(String[] args) { 
+int x,y,sum=0; 
+Scanner scan=new Scanner(System.in); 
+System.out.println("Enter the first number:"); x=scan.nextInt(); 
+System.out.println("Enter the second number:"); y=scan.nextInt(); 
+sum=(x+y); 
+System.out.println("The sum is" +" "+ +sum); 
+} 
+}
