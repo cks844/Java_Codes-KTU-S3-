7dc1f9677753f37d7c6a35139d26e29579106b4f
CYRIL  K  SONY <84973835+CyrilKSony@users.noreply.github.com>
2022-01-14 11:12:41 -0800

    Copying elements from one array to other

---
+++ Arrays(copying).java
@@ -0,0 +1,27 @@
+// To use Arrays.toString() method
+import java.util.Arrays;
+
+class Main {
+    public static void main(String[] args) {
+        int[] n1 = {2, 3, 12, 4, 12, -2};
+      
+        int[] n3 = new int[5];
+
+        // Creating n2 array of having length of n1 array
+        int[] n2 = new int[n1.length];
+      
+        // copying entire n1 array to n2
+        System.arraycopy(n1, 0, n2, 0, n1.length);
+        System.out.println("n2 = " +Arrays.toString(n2));  
+      
+        // copying elements from index 2 on n1 array
+        // copying element to index 1 of n3 array
+        // 2 elements will be copied
+        System.arraycopy(n1, 1, n3, 1, 3);
+        System.out.println("n3 = " + Arrays.toString(n3));  
+    }
+}
+
+OUTPUT:
+n2 = [2, 3, 12, 4, 12, -2]
+n3 = [0, 3, 12, 4, 0]
