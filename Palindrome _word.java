dd1d773ff95b4c2d187b01ed94306ff209d6ed91
CYRIL  K  SONY <84973835+CyrilKSony@users.noreply.github.com>
2022-01-14 23:28:15 -0800

    To check whether a word is palindrome or not

--- Palindrome _word.java
+++ Palindrome _word.java
@@ -3,11 +3,11 @@ class HelloWorld {
     public static void main(String[] args){
         Scanner s=new Scanner(System.in);
         String word,rev="";
-        int len,i;
+        int len,j;
         System.out.println("Enter the word:");
         word=s.next();
         len=word.length();
-        for(i=len-1;i>=0;i--)
+        for(j=len-1;j>=0;j--)
          rev=rev+word.charAt(i);
         if(word.equals(rev))
          System.out.println("The entered string is a palindrome word");
