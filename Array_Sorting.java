122d3b8805bec12e3bd2033ee9996b2a1004b077
CYRIL  K  SONY <84973835+cks844@users.noreply.github.com>
2022-04-02 22:03:45 -0700

    Program to perform sorting in an array

---
+++ Array_Sorting.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+
+class Main {
+    public static void main(String[] args) {
+        int[] n1 = {2, 3, 12, 4, 12, -2};
+        Arrays.sort(n1);
+        for(int list:n1){
+         System.out.println(list);
+        }
+    }
+}
